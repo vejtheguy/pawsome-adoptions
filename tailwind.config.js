781723b29e0f547ac0026c80d3c248a343d17025
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2023-12-09 14:42:35 -0700

    Update tailwind config extensions
    Added custom colors and font families. Custom min-width setting set as well.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        psWhite: "#FFFFFF",
+        psDarkGray: "#333333",
+        psMediumGray: "#7a7a7a",
+        psLightGray: "#F0F0F0",
+        psOrange: "#E07C24",
+        psBlue: "#7bc9ea",
+        psLightBlue: "#bdefff",
+        psLightGreen: "#dcffba",
+        psCream: "#f5f5dc",
+        psCoral: "#ff847c",
+        psLightCoral: "#ffe0de",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        flower: ["Indie Flower", "serif"],
+      },
+      minWidth: {
+        300: "300px",
+      },
+    },
   },
   plugins: [],
 };
