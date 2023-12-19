0c9f9cf207e8fa12444d2cefe8a2255edb4ea654
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2023-12-19 13:45:07 -0700

    Feat: create new dropdown for posts per page
    To keep the website more coherent, I created a custom dropdown to handle the posts per page values. Styling for dropdown will possibly be changed at some point.

--- src/components/postsPerPage.tsx
+++ src/components/postsPerPage.tsx
@@ -1,3 +1,7 @@
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef, useState } from "react";
+
 interface PostsPerPageProps {
   postsPerPage: number;
   handlePostsPerPage: (value: number) => void;
@@ -7,59 +11,88 @@ const PostsPerPage: React.FC<PostsPerPageProps> = ({
   postsPerPage,
   handlePostsPerPage,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const values: number[] = [25, 50, 75];
+
+  // Handles the boolean of when to open and close the dropdown
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Check if mouse click occurs outside of modal; trigger onClose if true
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  // Add event listener for clicking outside of of modal
+  useEffect(() => {
+    const handleDocumentClick = (event: MouseEvent) => {
+      handleClickOutside(event);
+    };
+
+    document.addEventListener("mousedown", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleDocumentClick);
+    };
+  }, []);
+
+  // Prevent background scrolling when the dropdown is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    // Makes sure overflow is set back to auto when unmounted
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  // Close dropdown once a selection is clicked
+  useEffect(() => {
+    setIsOpen(false);
+  }, [handlePostsPerPage]);
+
   return (
-    <div className="flex justify-start gap-3">
-      <div>
-        <input
-          type="radio"
-          id="25_items"
-          name="postsPerPage"
-          value="25"
-          checked={postsPerPage === 25}
-          className="hidden peer"
-          onChange={() => handlePostsPerPage(25)}
-        />
-        <label
-          htmlFor="25_items"
-          className="rounded-md cursor-pointer drop-shadow-md p-3 bg-white text-sm font-semibold text-psMediumGray hover:bg-psMediumGray hover:text-psWhite transition duration-200 peer-checked:border-psWhite peer-checked:text-psWhite peer-checked:bg-psCoral peer-checked:hover:bg-psMediumGray"
+    <div className="flex justify-center items-center gap-" ref={dropdownRef}>
+      <span className="mr-2 cursor-default">View:</span>
+      <span className="relative inline-block">
+        <button
+          type="button"
+          className={`flex justify-between items-center rounded-md p-3 text-sm font-semibold  hover:bg-psCoral hover:text-white transition duration-300 ${
+            isOpen ? "bg-psCoral text-white" : "bg-psLightGray text-psDarkGray"
+          }`}
+          id="options-menu"
+          onClick={handleToggle}
         >
-          25
-        </label>
-      </div>
-      <div>
-        <input
-          type="radio"
-          id="50_items"
-          name="postsPerPage"
-          value="50"
-          checked={postsPerPage === 50}
-          className="hidden peer"
-          onChange={() => handlePostsPerPage(50)}
-        />
-        <label
-          htmlFor="50_items"
-          className="rounded-md cursor-pointer drop-shadow-md p-3 bg-white text-sm font-semibold text-psMediumGray hover:bg-psMediumGray hover:text-psWhite transition duration-200 peer-checked:border-psWhite peer-checked:text-psWhite peer-checked:bg-psCoral peer-checked:hover:bg-psMediumGray"
-        >
-          50
-        </label>
-      </div>
-      <div>
-        <input
-          type="radio"
-          id="75_items"
-          name="postsPerPage"
-          value="75"
-          checked={postsPerPage === 75}
-          className="hidden peer"
-          onChange={() => handlePostsPerPage(75)}
-        />
-        <label
-          htmlFor="75_items"
-          className="rounded-md cursor-pointer drop-shadow-md p-3 bg-white text-sm font-semibold text-psMediumGray hover:bg-psMediumGray hover:text-psWhite transition duration-200 peer-checked:border-psWhite peer-checked:text-psWhite peer-checked:bg-psCoral peer-checked:hover:bg-psMediumGray"
-        >
-          75
-        </label>
-      </div>
+          {postsPerPage}
+          <FontAwesomeIcon icon={faAngleDown} size="sm" className="ml-2" />
+        </button>
+
+        {isOpen && (
+          <div className="p-2 w-full bg-psLightBlue font-poppins shadow-xl rounded-lg text-md text-center font-semibold text-psDarkGray absolute top-100 right-0 mt-2 z-20 cursor-pointer">
+            <ul className="flex flex-col w-full gap-2">
+              {values.map((value) => (
+                <li
+                  key={value}
+                  onClick={() => handlePostsPerPage(value)}
+                  className="hover:bg-psCoral hover:text-white bg-psWhite rounded-lg w-full p-2"
+                >
+                  {value}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </span>
     </div>
   );
 };
